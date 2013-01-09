0e7d8f5218800643115b6d127356949f4574a0e0
Julian Irwin <julian.irwin@gmail.com>
2013-01-08 21:32:24 -0600

    Added s2 (basically stub). I plan to add some adventures in this direction.

--- weatbag/tiles/s1.py
+++ weatbag/tiles/s1.py
@@ -1,6 +1,6 @@
 class Tile:
     def describe(self):
-        print("You push through the undergrowth - there's hardly a path here at all.")
+        print("You push through the undergrowth - the path is overgrown but continues on..")
     
     def action(self, player, do):
         # Nothing to do here yet.
