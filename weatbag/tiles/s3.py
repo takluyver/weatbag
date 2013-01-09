31ee9408bd3f8510ded20755f5a56cd6404aea73
Julian Irwin <julian.irwin@gmail.com>
2013-01-09 00:08:31 -0600

    Fleshed out s2

---
+++ weatbag/tiles/s3.py
@@ -0,0 +1,10 @@
+from weatbag import words
+
+class Tile:
+    def describe(self):
+        print( "The foliage along the path begins to thin and you "
+							 "notice that you are now climbing a gentle slope.")
+
+    def action(self, player, do):
+        # Nothing to do here yet.
+        print("Sorry, I don't understand")
