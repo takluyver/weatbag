dc0b9a548f9a78f3beeb5989f0682b0a819b5594
George Angelopoulos <l4than.d3vers@gmail.com>
2013-04-20 01:27:24 +0300

    Fixed leave bug in s1e1 and modified output text.

--- weatbag/tiles/s1e1.py
+++ weatbag/tiles/s1e1.py
@@ -11,11 +11,19 @@ class Tile:
             "It runs from South to North.")
 
         if self.contents['bug'] > 0:
-            print("You see a huge ugly bug rush past you, "
+            print("You see a huge, ugly bug rush past you, "
                 "following the river south.")
             self.contents['bug'] -= 1
         else:
-            print("You remember this is where you saw that bug going south")
+            print("You remember this is where you saw that bug going south.")
 
     def action(self, player, do):
         pass
+
+    def leave(self, player, direction):
+        if direction ==  's':
+            print("Bugs are the enemy and must be crushed!"
+                "So you decide to follow the bug upstream.")
+            return True
+        else:
+            return True
