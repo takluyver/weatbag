2cc3d7ac306ad7ae935acad7d4d338b6e00914c3
George Angelopoulos <l4than.d3vers@gmail.com>
2013-04-20 22:26:03 +0300

    small obnoxious change

--- weatbag/tiles/n1w1.py
+++ weatbag/tiles/n1w1.py
@@ -1,7 +1,7 @@
 class Tile:
     def describe(self):
         print("There is a small island northwest.\n"
-            "Unfortunatelly you can't swim so you need a transport to go there.\n"
+            "Unfortunatelly, you can't swim so you need a transport to go there.\n"
             "Luckilly to the north you see 2 children with a raft sitting "
             "under a tree." )
     
