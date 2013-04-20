fbdc3b4b0419547eb8a1534e278d97e1c2e2544f
Kleopatra Angelidi <akleop@gmail.com>
2013-04-20 19:18:03 +0300

    Added tile n1w1.

---
+++ weatbag/tiles/n1w1.py
@@ -0,0 +1,9 @@
+class Tile:
+    def describe(self):
+        print("There is a small island northwest.\n"
+            "Unfortunatelly you can't swim so you need a transport to go there.\n"
+            "Luckilly to the north you see 2 children with a raft sitting "
+            "under a tree." )
+    
+    def action(self, player, do):
+        pass
