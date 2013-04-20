1c657f1c63776ecbc57b47ebe329cf10ccc8967d
George Angelopoulos <l4than.d3vers@gmail.com>
2013-04-20 23:07:23 +0300

    Added the final bug tile. ToDo: leave restrictions

---
+++ weatbag/tiles/s2e1.py
@@ -0,0 +1,21 @@
+from weatbag import words
+
+class Tile:
+    def __init__(self):
+        self.first_time = True
+
+    def describe(self):
+        print("You have reached upstream."
+            "You see the bug.")
+
+    def action(self, player, do):
+        if (do[0] in words.look) and ('bug' in do):
+            print("Traceback (most recent call last):\n"
+                " File \"computergame.py\", line 97397, in matrix.py\n"
+                "IndexError: list index out of range")
+            input()
+            if self.first_time == True:
+                print("Suddenly, you realize!")
+                self.first_time = False
+                input()
+            print("It's not a bug, it's a feature!")
