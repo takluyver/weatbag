325e31b442f3e3cb7f984d26c030b4a6d87ff830
George Angelopoulos <l4than.d3vers@gmail.com>
2013-04-20 23:31:48 +0300

    leave restriction for s1

--- weatbag/tiles/s1.py
+++ weatbag/tiles/s1.py
@@ -6,3 +6,17 @@ class Tile:
     def action(self, player, do):
         # Nothing to do here yet.
         print("Sorry, I don't understand")
+
+    def leave(self, player, direction):
+        restricted_directions = { 'e' }
+
+        # I placed this restriction here to prevent 
+        # early entrance to the bug quest
+        # which should be started by going South from e1
+        if direction in restricted_directions:
+            print("The undergrowth in that direction "
+                "is impassable. You turn back.")
+            return False
+        else:
+            return True
+
