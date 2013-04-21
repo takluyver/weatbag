53510b2113afe79d1e6f07a1e46e414a9b64a2fe
Kleopatra Angelidi <akleop@gmail.com>
2013-04-22 02:49:15 +0300

    added text on describe

---
+++ weatbag/tiles/n3w4.py
@@ -0,0 +1,9 @@
+class Tile:
+   def describe(self):
+       print("Looks like you are in a walking mood because you still follow "
+       "the path. You hear birds singing, some small waves on the background, "
+       "there is a nice flowery smell, rays of sun make the tree sceenery "
+       "heavenly... I totaly get you, it's a nice path to have a walk!")
+                   
+   def action(self, player, do):
+       print("Sorry, I don't understand")
