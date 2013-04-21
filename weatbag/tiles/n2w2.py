ce8936e360c1d07a3aa72c24ca23b26449781329
Kleopatra Angelidi <akleop@gmail.com>
2013-04-22 01:47:18 +0300

    changed possision of previous tile/made new tile

--- weatbag/tiles/n2w2.py
+++ weatbag/tiles/n2w2.py
@@ -1,34 +1,15 @@
-from weatbag import words
-from weatbag.utils import transfer
-
 class Tile:
-    def __init__(self):
-        self.contents = {'doorknob': 1}
-    
-    def describe(self):
-        print("You are now on the island.")
-        if self.contents['doorknob']:
-            print("As you walk around you find something shiny on the ground. "
-            "It looks like a brass doorknob.")
-        else:
-            print("There is nothing interesting here")
-    def action(self, player, do):
-        if (do[0] in words.take) and ('doorknob' in do):
-            self.take_doorknob(player)
-            
-        else:
-            print("Sorry, I don't understand.")
-    
-    def take_doorknob(self, player):
-        if transfer('doorknob', self.contents, player.inventory):
-            print("You pick up the doorknob.")
-        else:
-            print("There is nothing here.")
-
-    def leave(self, player, direction):
-        if direction == "e":
-            print ("You can't go back by swimming, that part is full of "
-                 "electric eels.")
-            return False
-        else:
-            return True
+   def describe(self):
+       print("You are now on the island. In front of you there are some trees "
+       "and a small path. Feeling adventurous?")
+   
+   def action(self, player, do):
+       print("Sorry, I don't understand")
+       
+   def leave(self, player, direction):
+       if direction == "e":
+           print ("You can't go back by swimming, that part is full of "
+                "electric eels.")
+           return False
+       else:
+           return True
