f2ccd5bd0de5f158467a60a06edcf637a963a3d8
Kleopatra Angelidi <akleop@gmail.com>
2013-04-22 00:48:51 +0300

    added an item for the inventory

--- weatbag/tiles/n2w2.py
+++ weatbag/tiles/n2w2.py
@@ -3,22 +3,25 @@ from weatbag.utils import transfer
 
 class Tile:
     def __init__(self):
-        self.contents = {'key': 1}
+        self.contents = {'doorknob': 1}
     
     def describe(self):
         print("You are now on the island.")
-        if self.contents['key']:
-            print("As you walk around you find something shiny on the ground. Looks like a key.")
-    
+        if self.contents['doorknob']:
+            print("As you walk around you find something shiny on the ground. "
+            "It looks like a brass doorknob.")
+        else:
+            print("There is nothing interesting here")
     def action(self, player, do):
-        if (do[0] in words.take) and ('key' in do):
-            self.take_key(player)
+        if (do[0] in words.take) and ('doorknob' in do):
+            self.take_doorknob(player)
+            
         else:
             print("Sorry, I don't understand.")
     
-    def take_key(self, player):
-        if transfer('key', self.contents, player.inventory):
-            print("You pick up the key.")
+    def take_doorknob(self, player):
+        if transfer('doorknob', self.contents, player.inventory):
+            print("You pick up the doorknob.")
         else:
             print("There is nothing here.")
 
