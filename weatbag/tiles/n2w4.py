b6c6d0a0327f8067b68ebc723a5664c58e92c582
Kleopatra Angelidi <akleop@gmail.com>
2013-04-23 01:19:23 +0300

    added leave + boolean for describe + small fixes

--- weatbag/tiles/n2w4.py
+++ weatbag/tiles/n2w4.py
@@ -1,23 +1,30 @@
 from weatbag import words
 from weatbag.utils import transfer
 
-
 class Tile:
+    def __init__(self):
+        self.opendoor = False
+
     def describe(self):
-        print("You are standing in front of the hut's door. The hut looks very "
-              "small and you wonder who could live there... It has a door but "
-              "you can't open it because the doorknob is missing.") 
+        if self.opendoor:
+            print("The door is open and the kitties are playing with their "
+                  "momcat!")
+        else:
+            print("You are standing in front of the hut's door. The hut looks "
+                  "very small and you wonder who could live there... It has a "
+                  "door but you can't open it because the doorknob is missing.") 
     
     def action(self, player, do):
         if (do[0] in words.use) and do[1]=='doorknob':           
             if player.has('doorknob'):
                 player.take('doorknob') 
                 print("Doorknob fits fine, clicks and voila... Door is open!\n"
-                      "You are in a cozy living room with a cat and 5 kittens "
-                      "resting on a couch.\nThere is another room at west.")
-               
+                      "\nYou are in a cozy living room with a cat and 5 kittens"
+                      " resting on a couch.\nThere is another room at west.")
+                self.opendoor = True
             else:
                 print("You'll need a doorknob to open the door.")
+                self.opendoor = False
         else:
             print("Sorry, I don't understand.")    
             
@@ -27,5 +34,5 @@ class Tile:
             return False
         else:
             return True   
-                      
+                                  
 test_items = ['doorknob']
