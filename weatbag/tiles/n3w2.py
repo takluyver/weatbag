a0b3849ab9e7ac2c36c7dc27b30cf486f619db30
Kleopatra Angelidi <akleop@gmail.com>
2013-04-23 23:57:44 +0300

    corrected if cases / added catfood

--- weatbag/tiles/n3w2.py
+++ weatbag/tiles/n3w2.py
@@ -18,19 +18,26 @@ class Tile:
             print("There's the old fisherman with his boat, he can take you "
                   "across if you like!")
     
-    def action(self, player, do):
-        if (do[0] in words.use or do[0] in words.give) and do[1]=='catfood':
-
 #   I only added words.use because player might
 #   use the word "use" insteed of "give".
+    def action(self, player, do):
 
-            if player.has('catfood'):
-                player.take('catfood') 
-                print("Great! Now we can sail!")
-                self.give_catfood = True
+        if do[0] in words.use or do[0] in words.give:
+            if 'catfood' in do:
+                if player.has('catfood'):
+                    if self.give_catfood ==  False:
+                        player.take('catfood')
+                        self.give_catfood =  True
+                        print("Great! Now we can sail!")
+                    else:
+                        player.take('catfood')
+                        print("Oh how nice of you. You are the best!")
+            elif len(do) > 1:
+                print("I have no use for that.")
         else:
             print("Sorry, I don't understand.")
     
+
     def leave(self, player, direction):
         if direction=='e'and not self.give_catfood:
             print("You can't swim, remember the electric eels? You need a "
@@ -38,4 +45,5 @@ class Tile:
             return False
         return True
             
-test_items = ['catfood']
+#two catfoods!
+test_items = ['catfood','catfood']
