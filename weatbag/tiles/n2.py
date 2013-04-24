d5e0ae534a00b6f5439c7a427de1bb060370e05c
Thomas Kluyver <takowl@gmail.com>
2013-04-24 22:55:52 +0100

    Remove unnecessary part from if/elif statement

--- weatbag/tiles/n2.py
+++ weatbag/tiles/n2.py
@@ -13,9 +13,7 @@ class Tile:
             print("Someone has dropped a box of matches on the ground.")
     
     def leave(self, player, direction):
-        if direction=='n' and player.has('flaming torch'):
-            return True
-        elif direction=='n' and not player.has('flaming torch'):
+        if direction=='n' and not player.has('flaming torch'):
             print("You can't explore the cave without being able to see. "
             "You'll need that stalwart friend of the adventurer, "
             "the flaming torch.")
