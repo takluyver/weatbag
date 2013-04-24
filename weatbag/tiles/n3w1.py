17cbea2f24bcb5ab084ba0b70776490de29cb888
Kleopatra Angelidi <akleop@gmail.com>
2013-04-24 04:16:04 +0300

    added leave function for n and a wall of kitties! (OMG! kitty ascii!!1!11)

--- weatbag/tiles/n3w1.py
+++ weatbag/tiles/n3w1.py
@@ -6,12 +6,37 @@ class Tile:
         print("\nSorry, I don't understand")
         
     def leave(self, player, direction):
-        if direction=='w':
+        if direction == 'w':
             print("\nI'm afraid you can't go west.\nThe sea on that part is "
                   "fool of electric eels and you don't have a transport.")
             return False
-        elif direction=='e':
+        elif direction == 'e':
             print("\nThere is the side of a cave in front of you.\nYou can't go "
                   "east.")
+            return False
+        elif direction == 'n':
+            print("               ,               , \n"
+                  "             _/((             \)\ \n"
+                  "    _.---. .'   `\           /    '. .---._ \n"
+                  "  .'      `     ^ T=       =P ^     `      '. \n"
+                  " /     \       .--'         `--.       /     \ \n"
+                  "|      /       )'-.         .-'(       \      | \n"
+                  "; ,   <__..-(   '-.)       (.-'   )-..__>   , ; \n"
+                  " \ \-.__)    ``--._)       (_.--``    (__.-/ / \n"
+                  "  '.'-.__.-.                       .-.__.-'.' \n"
+                  "    '-...-'                        '-...-' \n"
+                  "               ,               , \n"
+                  "             _/((             \)\ \n"
+                  "    _.---. .'   `\           /    '. .---._ \n"
+                  "  .'      `     ^ T=       =P ^     `      '. \n"
+                  " /     \       .--'         `--.       /     \ \n"
+                  "|      /       )'-.         .-'(       \      | \n"
+                  "; ,   <__..-(   '-.)       (.-'   )-..__>   , ; \n"
+                  " \ \-.__)    ``--._)       (_.--``    (__.-/ / \n"
+                  "  '.'-.__.-.                       .-.__.-'.' \n"
+                  "    '-...-'                        '-...-' \n")
+            print("A wall of kitties doesn't allow you to go North.\nBut that "
+                  "doesn't make you mad... You can't be mad at kitties, "
+                  "rite?!\n")
             return False
         return True
