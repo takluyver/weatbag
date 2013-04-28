ae6c874d790825891aefbcfd366749eaeb2fcf05
Kleopatra Angelidi <akleop@gmail.com>
2013-04-29 01:48:18 +0300

    small indentation fix on n2w2

--- weatbag/tiles/n2w2.py
+++ weatbag/tiles/n2w2.py
@@ -1,18 +1,18 @@
 class Tile:
-   def describe(self):
-       print("You are now on the island. In front of you there are some trees "
-             "and a small path.\nFeeling adventurous?\n")
-   
-   def action(self, player, do):
-       print("Sorry, wat?!")
-       
-   def leave(self, player, direction):
-       if direction == "e":
-           print("You can't go back by swimming, that part is full of "
-                 "electric eels.\n")
-           return False
-       elif direction == "s":
-           print("I'm afraid I can't let you go there Dave.\n")
-           return False
-       else:
-           return True
+    def describe(self):
+        print("You are now on the island. In front of you there are some trees "
+              "and a small path.\nFeeling adventurous?\n")
+
+    def action(self, player, do):
+        print("Sorry, wat?!")
+
+    def leave(self, player, direction):
+        if direction == "e":
+            print("You can't go back by swimming, that part is full of "
+                  "electric eels.\n")
+            return False
+        elif direction == "s":
+            print("I'm afraid I can't let you go there Dave.\n")
+            return False
+        else:
+            return True
