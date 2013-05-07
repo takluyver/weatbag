d64782af2b35d29c783b5687482209f6b42651ff
Kleopatra Angelidi <akleop@gmail.com>
2013-05-07 22:27:33 +0300

    added description (n2w6)

--- weatbag/tiles/n2w6.py
+++ weatbag/tiles/n2w6.py
@@ -1,15 +1,16 @@
 class Tile:
 
     def describe(self):
-        print("blah blah describe\n")
+        print("Big rocks around the place, and the sound of small waves at the "
+              "weastern sea makes you calm! \n")
 
     def action(self, player, do):
         print("Sorry, I don't understand.\n")
 
     def leave(self, player, direction):
         if direction == "w":
-            print("My dear traveler, you can't go there, you will fall of the "
-                   "cliff!\n")
+            print("My dear traveler, you can't fall of the cliff! "
+                  "I won't let you do it.\n")
             return False
         elif direction == "n":
             print("It looks like the back of a wooden hut.\n"
