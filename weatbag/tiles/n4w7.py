73ecb39b3e5c27c9f91315735334da249fae48a9
Kleopatra Angelidi <akleop@gmail.com>
2013-05-12 22:25:37 +0300

    added describe on n4w7

--- weatbag/tiles/n4w7.py
+++ weatbag/tiles/n4w7.py
@@ -1,6 +1,30 @@
+from weatbag import words
+from weatbag.utils import transfer
+
 class Tile:
+
+    def __init__(self):
+        self.challenge_completed = False
+        
     def describe(self):
-        print("\n")
+        if challenge_completed:
+            print("The only thing around this place is sand, small rocks, the "
+                  "beach waters and about a dozen of kitties playing around.\n"
+                  "You aproach the kitties and to your surprise they all start "
+                  "rubbing at your legs and mewing! What is even more "
+                  "surprising is that your brain understands and translates "
+                  "meaws into words!\n"
+                  "You can clearly hear them say:\n"
+                  "'PLZ SIR, IT VRY HAWT HEER, CAN U TREAT US SUM  MICE "
+                  "CREAM?! ALL U NED IZ 2 COMBINE SUM MICE AN SUM CREAM AN "
+                  "READY IT IZ! PLEEEEASE!' *mieaw* *rub* *rub*\n")
+        else:
+            print("The only thing around this place is sand, small rocks, the "
+                  "beach waters and about a dozen of kitties playing around.\n")
 
     def action(self, player, do):
         print("Sorry, I don't understand.\n")
+        
+        
+test_items = ['mice', 'cream']
+
