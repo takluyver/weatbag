322416a21b70a38f4a1bd774109e10e95abe3793
Kleopatra Angelidi <akleop@gmail.com>
2013-05-12 21:36:28 +0300

    added new tiles n4w6 and n4w7

--- weatbag/tiles/n4w6.py
+++ weatbag/tiles/n4w6.py
@@ -1,6 +1,7 @@
 class Tile:
     def describe(self):
-        print("\n")
+        print("The beach makes a nice little curve on that spot. There are "
+              "some pine trees around too. A really quiet sceenery!\n")
 
     def action(self, player, do):
         print("Sorry, I don't understand.\n")
---
+++ weatbag/tiles/n4w7.py
@@ -0,0 +1,6 @@
+class Tile:
+    def describe(self):
+        print("\n")
+
+    def action(self, player, do):
+        print("Sorry, I don't understand.\n")
