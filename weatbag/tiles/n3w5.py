55beedbc868e89d6c124098c43f081ff0988369f
Kleopatra Angelidi <akleop@gmail.com>
2013-05-14 02:35:08 +0300

    added challenge (n3w5)

--- weatbag/tiles/n3w5.py
+++ weatbag/tiles/n3w5.py
@@ -6,12 +6,25 @@ class Tile:
         self.challenge_completed = False
         
     def describe(self):
-        print("\n")
-
+        print("Your feet do a crackling sound as your feet snapp some twigs on "
+              "the ground.\n")
+        if not self.challenge_completed:
+            self.challenge()
+            self.challenge_completed = True
+            
+    def challenge(self):
+        print("You just steped into something different. It is a metal box and "
+              "it looks like something is written on it. You clear the dirt "
+              "of it and what you see carved in the box lid is:\n\n"
+              "Stands\n"
+              "-------\n"
+              "0_2345\n\n"
+              "You try to open the box but it won't open.\n"
+              "Now obviously as you might have guessed already it is a magic "
+              "box! If you *understand* what these carvings represend the box "
+              "will open!")
+        # What the riddle says is "No one understands"
 
     def action(self, player, do):
-            print("\n")
-                self.challenge_completed = True
-            else:
-                print("I don't understand.")
+        print("I don't understand.")
 
