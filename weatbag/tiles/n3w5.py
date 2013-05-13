af8d37fddf448f96a465051e517fd79c197ecafc
Kleopatra Angelidi <akleop@gmail.com>
2013-05-14 01:41:36 +0300

    added new tile n3w5

---
+++ weatbag/tiles/n3w5.py
@@ -0,0 +1,17 @@
+from weatbag import words
+from weatbag.utils import transfer
+
+class Tile:
+    def __init__(self):
+        self.challenge_completed = False
+        
+    def describe(self):
+        print("\n")
+
+
+    def action(self, player, do):
+            print("\n")
+                self.challenge_completed = True
+            else:
+                print("I don't understand.")
+
