1e6a35c86949df1ff4794e27a6cdf9fee9567fc7
Kleopatra Angelidi <akleop@gmail.com>
2013-05-14 00:33:56 +0300

    changed describe added kitties() (n4w7)

--- weatbag/tiles/n4w7.py
+++ weatbag/tiles/n4w7.py
@@ -6,21 +6,22 @@ class Tile:
         self.challenge_completed = False
         
     def describe(self):
+        print("The only thing around this place is sand, small rocks, the "
+              "beach waters, about a dozen of kitties playing around and "
+              "a lighthouse on the north.\n")
         if not self.challenge_completed:
-            print("The only thing around this place is sand, small rocks, the "
-                  "beach waters and about a dozen of kitties playing around.\n"
-                  "You aproach the kitties and to your surprise they all start "
-                  "rubbing at your legs and mewing! What is even more "
-                  "surprising is that your brain understands and translates "
-                  "meaws into words!\n"
-                  "You can clearly hear them say:\n"
-                  "'PLZ SIR, IT VRY HAWT HEER, CAN U TREAT US SUM  MICE "
-                  "CREAM?! ALL U NED IZ 2 COMBINE SUM MICE AN SUM CREAM AN "
-                  "READY IT IZ! PLEEEEASE!' *mieaw* *rub* *rub*\n")
+            self.kitties()
             self.challenge_completed = True
-        else:
-            print("The only thing around this place is sand, small rocks, the "
-                  "beach waters and about a dozen of kitties playing around.\n")
+                  
+    def kitties(self):
+         print("You aproach the kitties and to your surprise they all start "
+               "rubbing at your legs and mewing! What is even more "
+               "surprising is that your brain understands and translates "
+               "meaws into words!\n"
+               "You can clearly hear them say:\n"
+               "'PLZ SIR, IT VRY HAWT HEER, CAN U TREAT US SUM  MICE "
+               "CREAM?! ALL U NED IZ 2 USE SUM MICE AN SUM CREAM AN "
+               "READY IT IZ! PLEEEEASE!' *mieaw* *rub* *rub*\n")
 
     def action(self, player, do):
             if do[0] in words.use and do[1] == "mice" and do[2]=="cream":
@@ -31,7 +32,7 @@ class Tile:
                     print("Balls of tasty omnom mice cream fall from your "
                          "hands! The kittens look sooooo pleased!")
                 else:
-                    print("You'll need some mice and some cream.")
+                    print("You'll need some mice and cream.")
 
     def leave(self, player, direction):
         if direction == "s":
@@ -42,6 +43,9 @@ class Tile:
             print("It's the open sea. You can't go anywhere near by "
                   "swimming.\n")
             return False
+        elif direction == "n":
+            print("You are heading to the lighthouse.")
+            return True
         else:
             return True
 
