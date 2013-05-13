21599326b293063f9477e325623e50d8df8af494
Kleopatra Angelidi <akleop@gmail.com>
2013-05-14 02:57:31 +0300

    added reply in the riddle in action (n3w5)

--- weatbag/tiles/n3w5.py
+++ weatbag/tiles/n3w5.py
@@ -3,6 +3,7 @@ from weatbag.utils import transfer
 
 class Tile:
     def __init__(self):
+        self.contents = {'knife': 1}
         self.challenge_completed = False
         
     def describe(self):
@@ -16,7 +17,7 @@ class Tile:
         print("You just steped into something different. It is a metal box and "
               "it looks like something is written on it. You clear the dirt "
               "of it and what you see carved in the box lid is:\n\n"
-              "Stands\n"
+              "stands\n"
               "-------\n"
               "0_2345\n\n"
               "You try to open the box but it won't open.\n"
@@ -26,5 +27,8 @@ class Tile:
         # What the riddle says is "No one understands"
 
     def action(self, player, do):
-        print("I don't understand.")
+        if do[0] == "no" and do[1] == "one" and do[2] == "understands":
+            print("And that is absolutely right!\n"
+                  "The lid opens and you see a brand new army knife!")
+
 
