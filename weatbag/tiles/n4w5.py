b9065cb31a2a1e39e0263b3c5d8d4b22f20c1d36
Kleopatra Angelidi <akleop@gmail.com>
2013-05-14 01:55:21 +0300

    added description and leave for east (n4w5)

--- weatbag/tiles/n4w5.py
+++ weatbag/tiles/n4w5.py
@@ -1,7 +1,14 @@
 class Tile:
     def describe(self):
-        print("\n")
+        print("There are not so many trees around but the ground is full of "
+              "pine needles and pine cones.\n")
 
     def action(self, player, do):
         print("I don't understand.")
-
+        
+    def leave(self, player, direction):
+        if direction == "e":
+            print("Area 51. No trespassing beyond this point.")
+            return False
+        else:
+            return True
