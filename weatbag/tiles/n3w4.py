a258846e826ddd029b440f56b0d690bdf4188d08
Kleopatra Angelidi <akleop@gmail.com>
2013-05-14 04:20:11 +0300

    changed a little bit the description (n3w4)

--- weatbag/tiles/n3w4.py
+++ weatbag/tiles/n3w4.py
@@ -1,9 +1,9 @@
 class Tile:
    def describe(self):
-       print("Looks like you are in a walking mood because you still follow "
-             "the path.\nYou hear birds singing, some small waves on the "
-             "background, there is a nice flowery smell, rays of sun make the "
-             "tree sceenery heavenly...\n"
+       print("Looks like you are in a walking mood!\n"
+             "You hear birds singing, some small waves on the background, "
+             "there is a nice flowery smell, rays of sun make the tree "
+             "sceenery heavenly...\n"
              "I totaly get you, it's a nice path to have a walk!\n")
                    
    def action(self, player, do):
