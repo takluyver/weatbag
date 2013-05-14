c46fa1f0e0d3a85802bbef0561be3a39ff4bdbcd
Kleopatra Angelidi <akleop@gmail.com>
2013-05-15 01:04:00 +0300

    imported words and transfer

--- weatbag/tiles/n5w5.py
+++ weatbag/tiles/n5w5.py
@@ -1,6 +1,12 @@
+from weatbag import words
+from weatbag.utils import transfer
+
 class Tile:
     def describe(self):
         print("\n")
 
     def action(self, player, do):
         print("I don't understand.")
+        
+        
+#sandy claws
--- weatbag/tiles/n5w7.py
+++ weatbag/tiles/n5w7.py
@@ -1,6 +1,11 @@
+from weatbag import words
+from weatbag.utils import transfer
+
 class Tile:
     def describe(self):
         print("\n")
 
     def action(self, player, do):
         print("I don't understand.")
+        
+#lighthouse
