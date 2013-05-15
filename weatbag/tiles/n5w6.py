5de4d9cd6da9923e960172719b7b42c6b3c2ba77
Kleopatra Angelidi <akleop@gmail.com>
2013-05-15 03:04:25 +0300

    describe and leave functions for n5w6

--- weatbag/tiles/n5w6.py
+++ weatbag/tiles/n5w6.py
@@ -1,6 +1,14 @@
 class Tile:
     def describe(self):
-        print("\n")
+        print("You walk in the beach. There is a light house on the west.\n")
 
     def action(self, player, do):
         print("I don't understand.")
+    
+    def leave(self, player, direction):
+        if direction == "n":
+            print("It's the open sea. You can't go anywhere near by "
+                  "swimming.\n")
+            return False
+        else:
+            return True
