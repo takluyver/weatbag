e8a63f4a16e00afaf4130e4bc0d4b8e4f94a75bc
Kleopatra Angelidi <akleop@gmail.com>
2013-05-15 03:30:16 +0300

    description for n5w7

--- weatbag/tiles/n5w7.py
+++ weatbag/tiles/n5w7.py
@@ -1,9 +1,21 @@
 from weatbag import words
-from weatbag.utils import transfer
 
 class Tile:
     def describe(self):
-        print("\n")
+        print("You are inside the light house. You hear a clicking sound and "
+              "the door closes behind you.\nIt is dark, and an enormous cat "
+              "sits on a computer compiling felinux kernels.\n"
+              "You try to open the door and leave but you realize it is "
+              "locked.\n"
+              "The cat turns at you and hisses loudly.\n"
+              "She grawls and because you now speak cat, you understand what "
+              "she says.\n\n"
+              "Puny human, how dare you disturb my peace!\n"
+              "If you tell a lie I will hang you; if you tell the truth I will "
+              "shoot you.\n\n"
+              "It looks like you are condemned... Though if you think a bit "
+              "more there is an apropriate answer that gets your ass out of "
+              "this hell!")
 
     def action(self, player, do):
         print("I don't understand.")
