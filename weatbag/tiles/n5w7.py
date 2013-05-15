c2a0d01ed46cb4bfea86f682d5637b7cd172376d
Kleopatra Angelidi <akleop@gmail.com>
2013-05-15 04:38:55 +0300

    added different description if challenge completed (n5w7)

--- weatbag/tiles/n5w7.py
+++ weatbag/tiles/n5w7.py
@@ -6,7 +6,7 @@ class Tile:
 
     def describe(self):
         if self.trapped:
-            print("You are inside the light house. You hear a clicking sound and "
+            print("You are inside the lighthouse. You hear a clicking sound and "
               "the door closes behind you.\nIt is dark, and an enormous cat "
               "sits on a computer compiling felinux kernels.\n"
               "You try to open the door and leave but you realize it is "
@@ -21,14 +21,34 @@ class Tile:
               "more there is an apropriate answer that gets your ass out of "
               "this hell!\n")
 
-
-    # The exact answer is 'You will hang me'. If she hang him, then the 
+    # The exact answer is 'You will hang me'. If she hang him, then the
     # statement was true and she could only hang him for telling a lie. 
-    # If she shoot him, then it makes the statement a lie and she was only 
-    # to shoot him for telling the truth. 
-    # An alternate solution is to say, "You will not shoot me," leading to the 
+    # If she shoot him, then it makes the statement a lie and she was only
+    # to shoot him for telling the truth.
+    # An alternate solution is to say, "You will not shoot me," leading to the
     # same quandary for the killercat.
+         
+        else:
+            print("You are inside the lighthouse. There's a computer in a "
+                  "corner and some printed papers on the ground with "
+                  "scriblings like:\n\n"
+                  "SO IM LIKE FIBBING WIT N OK?\n"
+                  "    LOL ITERATE FIBONACCI TERMS LESS THAN N /LOL\n"
+                  "    SO GOOD N BIG LIKE EASTERBUNNY\n"
+                  "    BTW, FIBONACCI LIKE BUNNIES! LOL\n"
+                  "    U BORROW CHEEZBURGER\n"
+                  "    U BORROW CHEEZBURGER\n"
+                  "    I CAN HAZ CHEEZBURGER\n"
+                  "    HE CAN HAZ CHEEZBURGER\n"
+                  "    WHILE I CUTE?\n"
+                  "        I AND HE CAN HAZ HE AND I ALONG WITH HE\n"
+                  "        IZ HE BIG LIKE N?\n"
+                  "            KTHXBYE\n"
+                  "        U BORROW HE\n\n"
+                  "Ah huh you think... lolpython fibonacci. Crazy catworks. "
+                  "Nothing to do here!\n")
 
+# http://www.dalkescientific.com/writings/diary/archive/2007/06/01/lolpython.html
 
     def action(self, player, do):
         if self.trapped:
@@ -54,7 +74,7 @@ class Tile:
                   "You are my prisoner!\n")
             return False
         elif direction in ("w", "n"):
-            print("It's the light house walls. What are you gonna do? "
+            print("It's the lighthouse walls. What are you gonna do? "
                   "Hit your head on them?\n")
             return False
         else:
